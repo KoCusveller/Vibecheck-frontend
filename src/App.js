7a0cb775e96ef8d555e8ceab544812dacfcc90b7
KoCus84 <ko.cusveller@gmail.com>
2021-06-28 14:42:49 +0200

    Pages created for home, play and post City

--- src/App.js
+++ src/App.js
@@ -11,40 +11,33 @@ import Login from "./pages/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
 import { getUserWithStoredToken } from "./store/user/actions";
-import { Jumbotron } from "react-bootstrap";
 
-const Home = () => (
-  <Jumbotron>
-    <h1>Home</h1>
-  </Jumbotron>
-);
-const Other = () => (
-  <Jumbotron>
-    <h1>Other</h1>
-  </Jumbotron>
-);
+import PostCity from "./pages/PostCity";
+import PlayCity from "./pages/PlayCity";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectAppLoading);
+	const dispatch = useDispatch();
+	const isLoading = useSelector(selectAppLoading);
 
-  useEffect(() => {
-    dispatch(getUserWithStoredToken());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(getUserWithStoredToken());
+	}, [dispatch]);
 
-  return (
-    <div className="App">
-      <Navigation />
-      <MessageBox />
-      {isLoading ? <Loading /> : null}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/other" component={Other} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navigation />
+			<MessageBox />
+			{isLoading ? <Loading /> : null}
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/PlayCity" component={PlayCity} />
+				<Route path="/PostCity" component={PostCity} />
+				<Route path="/signup" component={SignUp} />
+				<Route path="/login" component={Login} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
