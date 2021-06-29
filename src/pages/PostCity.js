dd175a53910a56bc939db9255bda741a5796b4ee
KoCus84 <ko.cusveller@gmail.com>
2021-06-29 16:29:46 +0200

    Post City form styling tweaks

--- src/pages/PostCity.js
+++ src/pages/PostCity.js
@@ -1,90 +1,112 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Col } from "react-bootstrap";
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 
 import { createCity } from "../store/cities/actions";
 
-export default function SignUp() {
-  const dispatch = useDispatch();
-  const history = useHistory();
+import { Form, Col, Button, Container } from "react-bootstrap";
 
-  const [name, set_name] = useState("");
-  const [imgUrl, set_imgUrl] = useState("");
-  const [vidUrl, set_vidUrl] = useState("");
-  const [songUrl, set_songUrl] = useState("");
+export default function PostCity() {
+	const dispatch = useDispatch();
+	const history = useHistory();
 
-  function submitForm(event) {
-    event.preventDefault();
+	const [name, set_name] = useState("");
+	const [imgUrl, set_imgUrl] = useState("");
+	const [vidUrl, set_vidUrl] = useState("");
+	const [songUrl, set_songUrl] = useState("");
 
-    dispatch(createCity(name, imgUrl, vidUrl, songUrl));
+	function submitForm(event) {
+		event.preventDefault();
 
-    set_name("");
-    set_imgUrl("");
-    set_vidUrl("");
-    set_songUrl("");
+		dispatch(createCity(name, imgUrl, vidUrl, songUrl));
 
-    history.push("/");
-  }
+		set_name("");
+		set_imgUrl("");
+		set_vidUrl("");
+		set_songUrl("");
 
-  return (
-    <div>
-      <Container>
-        <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-          <h1 className="mt-5 mb-5">Post a new Vibecheck</h1>
-          <Form.Group controlId="formBasicName">
-            <Form.Label>City Name</Form.Label>
-            <Form.Control
-              value={name}
-              onChange={(event) => set_name(event.target.value)}
-              type="text"
-              placeholder="Enter City"
-              required
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicImgUrl">
-            <Form.Label>City Image Url</Form.Label>
-            <Form.Control
-              value={imgUrl}
-              onChange={(event) => set_imgUrl(event.target.value)}
-              type="text"
-              placeholder="Enter Url"
-              required
-            />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
+		history.push("/");
+	}
 
-          <Form.Group controlId="formBasicVidUrl">
-            <Form.Label>Enter City Video Url</Form.Label>
-            <Form.Control
-              value={vidUrl}
-              onChange={(event) => set_vidUrl(event.target.value)}
-              type="text"
-              placeholder="Enter Url"
-              required
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicSongUrl">
-            <Form.Label>Enter Song Url</Form.Label>
-            <Form.Control
-              value={songUrl}
-              onChange={(event) => set_songUrl(event.target.value)}
-              type="text"
-              placeholder="Enter Url"
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mt-5">
-            <Button variant="primary" type="submit" onClick={submitForm}>
-              I'm ready to vibe
-            </Button>
-          </Form.Group>
-        </Form>
-      </Container>
-    </div>
-  );
+	return (
+		<div>
+			<Container>
+				<Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+					<h1 className="mt-5 mb-5">Post a new Vibecheck</h1>
+
+					<Form.Group controlId="formBasicName">
+						<Form.Control
+							value={name}
+							onChange={(event) => set_name(event.target.value)}
+							onFocus={(event) => (event.target.placeholder = "")}
+							onBlur={(event) =>
+								(event.target.placeholder =
+									"Please enter City Name")
+							}
+							type="text"
+							placeholder="Please enter City Name"
+							required
+						/>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicImageUrl">
+						<Form.Control
+							value={imgUrl}
+							onChange={(event) => set_imgUrl(event.target.value)}
+							onFocus={(event) => (event.target.placeholder = "")}
+							onBlur={(event) =>
+								(event.target.placeholder =
+									"Please enter Image Url")
+							}
+							type="text"
+							placeholder="Please enter Image Url"
+							required
+						/>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicVidUrl">
+						<Form.Control
+							value={vidUrl}
+							onChange={(event) => set_vidUrl(event.target.value)}
+							onFocus={(event) => (event.target.placeholder = "")}
+							onBlur={(event) =>
+								(event.target.placeholder =
+									"Please enter Video Url")
+							}
+							type="text"
+							placeholder="Please enter Video Url"
+							required
+						/>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicSongUrl">
+						<Form.Control
+							value={songUrl}
+							onChange={(event) =>
+								set_songUrl(event.target.value)
+							}
+							onFocus={(event) => (event.target.placeholder = "")}
+							onBlur={(event) =>
+								(event.target.placeholder =
+									"Please enter Song Url")
+							}
+							type="text"
+							placeholder="Please enter Song Url"
+							required
+						/>
+					</Form.Group>
+
+					<Form.Group className="mt-5">
+						<Button
+							variant="primary"
+							type="submit"
+							onClick={submitForm}
+						>
+							I'm ready to vibe
+						</Button>
+					</Form.Group>
+				</Form>
+			</Container>
+		</div>
+	);
 }
