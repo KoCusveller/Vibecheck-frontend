0af0e2efc9d1c0fb482ea9d37bd87323c188b062
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-06-29 16:08:59 +0200

    nature sounds working. Problem with volume

--- src/components/NatureSounds.js
+++ src/components/NatureSounds.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useState } from "react";
 import ReactPlayer from "react-player";
 
+//Object that stores all the Nature Sounds information
 const type = {
   JUNGLE: {
     name: "JUNGLE",
@@ -13,27 +14,33 @@ const type = {
   STORM: { name: "STORM", url: "https://www.youtube.com/watch?v=nDq6TstdEi8" },
   NONE: { name: "NONE", url: "" },
 };
+
 export default function NatureSounds() {
   const [natureSound, setNatureSound] = useState(type.NONE);
   const [playing, setplaying] = useState(false);
   const [volume, setVolume] = useState(1);
 
   function handleSound(sound) {
-    if (sound.name === natureSound.name) setplaying(!playing);
+    //If you click twice the same button toggle pause/play
+    if (sound.name === natureSound.name) {
+      return setplaying(!playing);
+    }
 
+    //else set new sound and start playing
     setNatureSound(type[sound.name]);
+    setplaying(true);
   }
 
   return (
-    <div style={{ display: "none" }}>
+    <div>
       <ReactPlayer
+        style={{ display: "none" }}
         url={natureSound.url}
         volume={volume}
         playing={playing}
         className="react-player"
         loop={true}
       />
-
       <div className="nature-sound-buttons">
         <button onClick={() => handleSound(type.JUNGLE)}>JUGLE INCON</button>
         <button onClick={() => handleSound(type.OCEAN)}>OCEAN INCON</button>
@@ -42,9 +49,9 @@ export default function NatureSounds() {
         <input
           type="range"
           min={0}
-          max={10}
+          max={100}
           onChange={(e) => {
-            setVolume(parseInt(e.target.value / 10));
+            setVolume(parseInt(e.target.value));
           }}
         />
       </div>
