250bc460db61e2643e2b940f9b1ee64d7a5efc58
KoCus84 <ko.cusveller@gmail.com>
2021-06-30 14:16:18 +0200

    start creating homepage city layout

--- src/components/CityMiniature.js
+++ src/components/CityMiniature.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Card, Image } from "react-bootstrap";
 
-// window frame style/size
-
-// get name & url props, map it
+import "./Style/CityMiniature.css";
 
 export default function CityMiniature({ id, name, url }) {
-  return (
-    <div>
-      <button>
-        {name}
-        <Link to={`/PlayCity/${id}`}>
-          <img src={url} alt={name} style={{ maxWidth: "400px" }} />
-        </Link>
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<Card className="cardCity">
+				<Card.Body>
+					<button className="cardButton">
+						<Card.Title>{name}</Card.Title>
+
+						<Link to={`/PlayCity/${id}`}>
+							<Image
+								className="cardImage"
+								src={url}
+								alt={name}
+								variant="bottom"
+								fluid
+							/>
+						</Link>
+					</button>
+				</Card.Body>
+			</Card>
+		</div>
+	);
 }
