f764c966868da40f6eb113563f5712bb0e106240
KoCus84 <ko.cusveller@gmail.com>
2021-07-02 11:37:06 +0200

    logout now redirects to home

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -5,22 +5,30 @@ import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
 import Nav from "react-bootstrap/Nav";
 import NavbarItem from "./NavbarItem";
+import { useHistory } from "react-router";
 
 export default function LoggedIn() {
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
-  return (
-    <>
-      <NavbarItem path="/PostCity" linkText="Post City Vibe" />
-      <Nav.Item style={{ padding: ".5rem 1rem", fontWeight: "bold" }}>
-        {user.name}
-      </Nav.Item>
-      <Button
-        onClick={() => dispatch(logOut())}
-        style={{ color: "rgb(201, 194, 206)", fontWeight: "bold" }}
-      >
-        Logout
-      </Button>
-    </>
-  );
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const user = useSelector(selectUser);
+
+	function logout() {
+		dispatch(logOut());
+		history.push("/");
+	}
+
+	return (
+		<>
+			<NavbarItem path="/PostCity" linkText="Post City Vibe" />
+			<Nav.Item style={{ padding: ".5rem 1rem", fontWeight: "bold" }}>
+				{user.name}
+			</Nav.Item>
+			<Button
+				onClick={() => logout()}
+				style={{ color: "rgb(201, 194, 206)", fontWeight: "bold" }}
+			>
+				Logout
+			</Button>
+		</>
+	);
 }
